eed71bbcbeaeeae97c74af4b958b59c124cb29b9
auliarahmat <auliarahmat25599@gmail.com>
2022-06-23 21:28:36 +0700

    first commit

---
+++ components/Banner.vue
@@ -0,0 +1,272 @@
+<template>
+  <div class="relative bg-blue-400">
+    <div class="mx-auto py-8 px-4 sm:py-14 sm:px-4 lg:max-w-7xl lg:px-8">
+      <img
+        src="https://caribarang.oss-ap-southeast-1.aliyuncs.com/general/lp/caribarang-logo.png"
+        alt="caribarang"
+        class="mx-auto"
+      />
+
+      <div class="sm:w-10/12 mx-auto text-center text-white py-2 sm:py-6">
+        <h1 class="text-xl sm:text-6xl font-bold capitalize leading-tight">
+          Jadi pedagang online internasional dengan amazon FBA, Belajar langsung
+          dari ahlinya
+        </h1>
+        <p class="mt-3 sm:text-xl italic text-base">
+          <strong
+            >(It's your turn to become a worldwide online seller!<br />Start
+            selling from all around the world with Amazon FBA. Learn more from
+            the expert!)</strong
+          >
+        </p>
+      </div>
+
+      <div
+        class="w-full sm:w-10/12 mx-auto text-center text-white py-3 sm:py-6"
+      >
+        <p class="mt-6 sm:text-2xl text-base">
+          <strong>5000+ orang</strong> sudah mengikuti seminar ini yang diadakan
+          di berbagai negara asia seperti Singapura, Filipina, Malaysia, Taiwan,
+          dan Hongkong
+        </p>
+        <p class="mt-6 sm:text-xl italic text-base">
+          (Be part of 5.000+ people who successfully join our seminar from
+          Southeast Asia, such as Singapore,<br />Malaysia, Taiwan, Hongkong,
+          and Phillipines.)
+        </p>
+      </div>
+
+      <div class="grid grid-cols-1 lg:grid-cols-7 bg-white rounded-lg mt-8">
+        <div
+          class="
+            border-r border-gray-400
+            md:py-5
+            flex flex-col
+            justify-center
+            items-center
+          "
+        >
+          <span class="text-red-600 sm:text-2xl">Tuesday</span>
+          <span class="text-gray-800 text-sm sm:text-3xl"
+            ><strong>28</strong></span
+          >
+          <span class="mt-0 sm:mt-2 text-gray-500 text-sm sm:text-sm"
+            >June 2022</span
+          >
+          <span class="mt-0 sm:mt-2 font-bold text-sm sm:text-sm"
+            >14.00 - 17.00 WIB</span
+          >
+          <span class="mt-0 sm:mt-2 text-gray-700 text-sm sm:text-sm"
+            >Hotel Aston Kemayoran</span
+          >
+        </div>
+
+        <div
+          class="
+            border-r border-gray-400
+            md:py-2
+            flex flex-col
+            justify-center
+            items-center
+          "
+        >
+          <span class="text-red-600 sm:text-2xl">Tuesday</span>
+          <span class="text-gray-800 text-sm sm:text-3xl"
+            ><strong>28</strong></span
+          >
+          <span class="mt-0 sm:mt-2 text-gray-500 text-sm sm:text-sm"
+            >June 2022</span
+          >
+          <span class="mt-0 sm:mt-2 font-bold text-sm sm:text-sm"
+            >14.00 - 17.00 WIB</span
+          >
+          <span class="mt-0 sm:mt-2 text-gray-700 text-sm sm:text-sm"
+            >Hotel Aston Kemayoran</span
+          >
+        </div>
+
+        <div
+          class="
+            border-r border-gray-400
+            md:py-2
+            flex flex-col
+            justify-center
+            items-center
+          "
+        >
+          <span class="text-red-600 sm:text-2xl">Tuesday</span>
+          <span class="text-gray-800 text-sm sm:text-3xl"
+            ><strong>28</strong></span
+          >
+          <span class="mt-0 sm:mt-2 text-gray-500 text-sm sm:text-sm"
+            >June 2022</span
+          >
+          <span class="mt-0 sm:mt-2 font-bold text-sm sm:text-sm"
+            >14.00 - 17.00 WIB</span
+          >
+          <span class="mt-0 sm:mt-2 text-gray-700 text-sm sm:text-sm"
+            >Hotel Aston Kemayoran</span
+          >
+        </div>
+
+        <div
+          class="
+            border-r border-gray-400
+            md:py-2
+            flex flex-col
+            justify-center
+            items-center
+          "
+        >
+          <span class="text-red-600 sm:text-2xl">Tuesday</span>
+          <span class="text-gray-800 text-sm sm:text-3xl"
+            ><strong>28</strong></span
+          >
+          <span class="mt-0 sm:mt-2 text-gray-500 text-sm sm:text-sm"
+            >June 2022</span
+          >
+          <span class="mt-0 sm:mt-2 font-bold text-sm sm:text-sm"
+            >14.00 - 17.00 WIB</span
+          >
+          <span class="mt-0 sm:mt-2 text-gray-700 text-sm sm:text-sm"
+            >Hotel Aston Kemayoran</span
+          >
+        </div>
+
+        <div
+          class="
+            border-r border-gray-400
+            md:py-2
+            flex flex-col
+            justify-center
+            items-center
+          "
+        >
+          <span class="text-red-600 sm:text-2xl">Tuesday</span>
+          <span class="text-gray-800 text-sm sm:text-3xl"
+            ><strong>28</strong></span
+          >
+          <span class="mt-0 sm:mt-2 text-gray-500 text-sm sm:text-sm"
+            >June 2022</span
+          >
+          <span class="mt-0 sm:mt-2 font-bold text-sm sm:text-sm"
+            >14.00 - 17.00 WIB</span
+          >
+          <span class="mt-0 sm:mt-2 text-gray-700 text-sm sm:text-sm"
+            >Hotel Aston Kemayoran</span
+          >
+        </div>
+
+        <div
+          class="
+            border-r border-gray-400
+            md:py-2
+            flex flex-col
+            justify-center
+            items-center
+          "
+        >
+          <span class="text-red-600 sm:text-2xl">Tuesday</span>
+          <span class="text-gray-800 text-sm sm:text-3xl"
+            ><strong>28</strong></span
+          >
+          <span class="mt-0 sm:mt-2 text-gray-500 text-sm sm:text-sm"
+            >June 2022</span
+          >
+          <span class="mt-0 sm:mt-2 font-bold text-sm sm:text-sm"
+            >14.00 - 17.00 WIB</span
+          >
+          <span class="mt-0 sm:mt-2 text-gray-700 text-sm sm:text-sm"
+            >Hotel Aston Kemayoran</span
+          >
+        </div>
+
+        <div
+          class="
+            border-r border-gray-400
+            md:py-2
+            flex flex-col
+            justify-center
+            items-center
+          "
+        >
+          <span class="text-red-600 sm:text-2xl">Tuesday</span>
+          <span class="text-gray-800 text-sm sm:text-3xl"
+            ><strong>28</strong></span
+          >
+          <span class="mt-0 sm:mt-2 text-gray-500 text-sm sm:text-sm"
+            >June 2022</span
+          >
+          <span class="mt-0 sm:mt-2 font-bold text-sm sm:text-sm"
+            >14.00 - 17.00 WIB</span
+          >
+          <span class="mt-0 sm:mt-2 text-gray-700 text-sm sm:text-sm"
+            >Hotel Aston Kemayoran</span
+          >
+        </div>
+      </div>
+
+      <div
+        class="max-w-2xl mx-auto py-5 px-4 sm:py-5 sm:px-4 lg:max-w-7xl lg:px-5"
+      >
+        <div class="w-full sm:w-6/12 mx-auto text-center text-gray-500">
+          <a href="https://goo.gl/maps/wHDz3K6BsGopKqNGA" target="_blank"
+            ><h1
+              class="
+                text-md
+                sm:text-md
+                font-medium
+                leading-tight
+                capitalize
+                text-white
+              "
+            >
+              <b>Lokasi Seminar</b>: Aston Kemayoran City Hotel (Ruangan
+              Mentawai dan Raja Ampat Lt.25) <br />
+              Jl. HBR Motik No.4, RW.10, Gn. Sahari Sel., Kec. Kemayoran,
+              Jakarta Pusat.
+            </h1></a
+          >
+        </div>
+      </div>
+
+      <div class="absolute -bottom-5 sm:-bottom-8 button-register">
+        <button
+          class="
+            flex
+            bg-red-500
+            text-white
+            active:bg-red-600
+            font-bold
+            capitalize
+            text-sm
+            sm:text-lg
+            px-6
+            sm:px-12
+            py-2
+            sm:py-4
+            rounded-xl
+            shadow-md
+            hover:shadow-lg
+            outline-none
+            focus:outline-none
+            mr-1
+            mb-1
+            ease-linear
+            transition-all
+            duration-150
+            border border-white
+          "
+        >
+          Register Now
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "banner",
+};
+</script>
