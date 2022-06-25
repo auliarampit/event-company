d34f52a32774d7d1a08abc7d541ab3dd2e9f63b9
auliarahmat <auliarahmat25599@gmail.com>
2022-06-25 15:09:51 +0700

    finish content

---
+++ components/Testimoni.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="bg-white py-6">
+    <div class="max-w-2xl mx-auto lg:max-w-7xl lg:px-8">
+      <div class="w-10/12 sm:w-6/12 mx-auto text-center text-gray-700 sm:py-6">
+        <h1 class="text-2xl sm:text-5xl font-bold leading-tight">
+          Mau Seperti Mereka? Daftar Sekarang
+        </h1>
+        <p
+          class="
+            text-gray-400
+            font-bold
+            italic
+            text-base
+            sm:text-lg
+            text-center
+            mt-2
+          "
+        >
+          (Want to earn from your own international online store?<br />Register
+          Now!)
+        </p>
+      </div>
+    </div>
+
+    <div class="hidden sm:block w-full relative mt-8 mb-10">
+      <div class="bg-red-500 testy-bg"></div>
+      <div class="max-w-2xl mx-auto lg:max-w-7xl lg:px-8 z-10 relative">
+        <div class="grid grid-cols-4">
+          <div class="px-6">
+            <div
+              class="
+                bg-white
+                rounded-t-lg
+                shadow-lg
+                rounded-b-lg
+                overflow-hidden
+              "
+            >
+              <img
+                src="https://caribarang.oss-ap-southeast-1.aliyuncs.com/banners/5.jpg"
+              />
+              <div class="bg-white w-full text-center py-2 flex flex-col">
+                <span class="text-2xl text-gray-700">78.58K USD</span>
+                <span class="text-gray-400">per month</span>
+              </div>
+            </div>
+          </div>
+          <div class="px-6">
+            <div
+              class="
+                bg-white
+                rounded-t-lg
+                shadow-lg
+                rounded-b-lg
+                overflow-hidden
+                scale-16
+              "
+            >
+              <img
+                src="https://caribarang.oss-ap-southeast-1.aliyuncs.com/banners/7.jpg"
+              />
+              <div class="bg-white w-full text-center py-2 flex flex-col">
+                <span class="text-2xl text-gray-700">120.1K USD</span>
+                <span class="text-gray-400">per month</span>
+              </div>
+            </div>
+          </div>
+          <div class="px-6">
+            <div
+              class="
+                bg-white
+                rounded-t-lg
+                shadow-lg
+                rounded-b-lg
+                overflow-hidden
+                scale-16
+              "
+            >
+              <img
+                src="https://caribarang.oss-ap-southeast-1.aliyuncs.com/banners/8.jpg"
+              />
+              <div class="bg-white w-full text-center py-2 flex flex-col">
+                <span class="text-2xl text-gray-700">102.6K USD</span>
+                <span class="text-gray-400">per month</span>
+              </div>
+            </div>
+          </div>
+          <div class="px-6">
+            <div
+              class="
+                bg-white
+                rounded-t-lg
+                shadow-lg
+                rounded-b-lg
+                overflow-hidden
+              "
+            >
+              <img
+                src="https://caribarang.oss-ap-southeast-1.aliyuncs.com/banners/6.jpg"
+              />
+              <div class="bg-white w-full text-center py-2 flex flex-col">
+                <span class="text-2xl text-gray-700">48.12 USD</span>
+                <span class="text-gray-400">per month</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Testimoni",
+};
+</script>
+
+<style>
+.background {
+  position: absolute;
+  top: 50%;
+  width: "100%";
+  height: 240;
+  transform: translateY(-50%);
+  background-color: red;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,186 @@
     <Banner />
     <Amazon />
     <Learn />
+    <Testimoni />
+
+    <div class="section-question bg-gray-200">
+      <div
+        class="
+          max-w-2xl
+          mx-auto
+          py-8
+          px-4
+          sm:py-10 sm:px-4
+          lg:max-w-7xl lg:px-8
+        "
+      >
+        <div class="w-full sm:w-6/12 mx-auto text-center text-gray-700">
+          <h1 class="text-xl sm:text-4xl font-bold leading-tight capitalize">
+            Frequently asked question
+          </h1>
+        </div>
+        <div class="py-6 grid grid-cols-1 sm:grid-cols-2 gap-3 mt-5">
+          <div>
+            <h3 class="text-gray-600 text-base sm:text-xl font-bold">
+              Mengapa menjual barang di marketplace Amazon?
+            </h3>
+            <p class="text-gray-400 italic text-base sm:text-lg font-bold">
+              (Why sell on a Marketplace like Amazon?)
+            </p>
+            <div class="flex py-3 text-gray-500 text-sm sm:text-base">
+              <span class="pr-2"><i class="fas fa-play"></i></span>
+              <p>
+                Menjual di marketplace adalah tren bisnis E-Commerce yang sangat
+                besar. Biaya operasional seperti tenaga kerja, logistik, iklan,
+                dan biaya lain nya dalam e-Commerce dapat di tekan seminimal
+                mungkin. Yang paling hebatnya, ini adalah bisnis yang dapat
+                tumbuh dan di operasikan darimana saja di dunia.
+              </p>
+            </div>
+            <div class="flex py-3 pl-5 text-gray-500 text-sm sm:text-base">
+              <p class="italic">
+                (Selling on a Marketplace is a huge e-commerce business trend
+                now. The whole business system is automated. Operating costs
+                such as manpower logistics, advertising and other expenses in
+                e-commerce can be kept to a minimum. The best part? This is a
+                business that can grow and be operated from anywhere in the
+                world.)
+              </p>
+            </div>
+          </div>
+          <div>
+            <h3 class="text-gray-600 text-base sm:text-xl font-bold">
+              Apakah saya membutuhkan banyak modal untuk memulai bisnis online
+              ini?
+            </h3>
+            <p class="text-gray-400 italic text-base sm:text-lg font-bold">
+              (Do I need a lot of capital to start this online business?)
+            </p>
+            <div class="flex py-3 text-gray-500 text-sm sm:text-base">
+              <span class="pr-2"><i class="fas fa-play"></i></span>
+              <p>
+                Tidak kok, Amazon FBA dapat berjalan dengan modal minimal karena
+                sebagian besar layanannya gratis, kunci sebenarnya terletak pada
+                pemilihan produk yang tepat dijual di platform ini.
+              </p>
+            </div>
+            <div class="flex py-3 pl-5 text-gray-500 text-sm sm:text-base">
+              <p class="italic">
+                (Unlike a brick-and-mortar business, an Amazon FBA can get up
+                and running with minimal capital as most of the services are
+                free. The key really lies in selecting the right products to
+                sell on this platform.)
+              </p>
+            </div>
+          </div>
+          <div>
+            <h3 class="text-gray-600 text-base sm:text-xl font-bold">
+              Dapatkah saya memulai bisnis Amazon FBA bahkan tanpa latar
+              belakang TI dan perangkat lunak?
+            </h3>
+            <p class="text-gray-400 italic text-base sm:text-lg font-bold">
+              (Can I start an Amazon FBA business even with no IT and software
+              background?)
+            </p>
+            <div class="flex py-3 text-gray-500 text-sm sm:text-base">
+              <span class="pr-2"><i class="fas fa-play"></i></span>
+              <p>
+                Amazon memudahkan penjual tanpa keahlian TI atau pengetahuan
+                pengembangan perangkat lunak untuk mengoperasikan bisnis.
+                Faktanya, kamu memiliki siswa berusia diatas 70 tahun yang
+                menjual di platform ini. Hanya untuk menunjukkan bahwa itu
+                mungkin bagi siapa saja yang mau berusaha.
+              </p>
+            </div>
+            <div class="flex py-3 pl-5 text-gray-500 text-sm sm:text-base">
+              <p class="italic">
+                (Amazon makes it easy for sellers without IT expertise or
+                software development knowledge to operate the business in fact,
+                we have students over the age of 70 selling on this platform..
+                just to show it's possible for anyone who's willing to put in
+                some effort)
+              </p>
+            </div>
+          </div>
+          <div>
+            <h3 class="text-gray-600 text-base sm:text-xl font-bold">
+              Bagaimana orang akan menemukan produk saya jika saya tidak
+              beriklan?
+            </h3>
+            <p class="text-gray-400 italic text-base sm:text-lg font-bold">
+              (How would people find my products if I don't advertise?)
+            </p>
+            <div class="flex py-3 text-gray-500 text-sm sm:text-base">
+              <span class="pr-2"><i class="fas fa-play"></i></span>
+              <p>
+                Amazon memiliki ratusan juta pembeli yang secara aktif mencari
+                dan membeli produk. ada banyak strategi yang bisa dilakukan
+                tanpa mengeluarkan biaya untuk iklan. misalnya, memastikan bahwa
+                deskripsi produk kami ditulis dengan cara yang benar. kami akan
+                membahas ini dan strategi lainnya diseminar ini.
+              </p>
+            </div>
+            <div class="flex py-3 pl-5 text-gray-500 text-sm sm:text-base">
+              <p class="italic">
+                (Amazon has several hundred million buyers actively seeking and
+                buying products on a regular basis. There are many strategies to
+                get found without spending an ads... for example, making sure
+                that our listings are written in the right way. We'll discuss
+                this and other strategies in our upcoming webinar.)
+              </p>
+            </div>
+          </div>
+          <div>
+            <h3 class="text-gray-600 text-base sm:text-xl font-bold">
+              Dalam bahasa apa seminarnya akan dibawakan?
+            </h3>
+            <p class="text-gray-400 italic text-base sm:text-lg font-bold">
+              (In what language will the seminar be delivered?)
+            </p>
+            <div class="flex py-3 text-gray-500 text-sm sm:text-base">
+              <span class="pr-2"><i class="fas fa-play"></i></span>
+              <p>
+                Seminarnya akan dibawakan dalam bahasa inggris, tetapi tenang
+                saja, akan ada terjemahan bahasa Indonesia pada setiap slide
+                presentasi
+              </p>
+            </div>
+            <div class="flex py-3 pl-5 text-gray-500 text-sm sm:text-base">
+              <p class="italic">
+                (The seminar will be delivered in English, but don worry, there
+                will be an Indonesian translation on each presentation slide. We
+                make it easy for you!)
+              </p>
+            </div>
+          </div>
+          <div>
+            <h3 class="text-gray-600 text-base sm:text-xl font-bold">
+              Apakah seminar ini mengikuti protokol kesehatan?
+            </h3>
+            <p class="text-gray-400 italic text-base sm:text-lg font-bold">
+              (Is this event follow the health protocol?)
+            </p>
+            <div class="flex py-3 text-gray-500 text-sm sm:text-base">
+              <span class="pr-2"><i class="fas fa-play"></i></span>
+              <p>
+                Iya, kami mengikuti peraturan protokol kesehatan yang berlaku
+                dengan memberi jarak antar peserta, membatasi kapasitas ruangan
+                dan peraturan protokol kesehatan lainnya.
+              </p>
+            </div>
+            <div class="flex py-3 pl-5 text-gray-500 text-sm sm:text-base">
+              <p class="italic">
+                (Yes, we follow the applied health protocol regulation such as ;
+                social distancing, limiting room capacity, and other health
+                protocol regulations.)
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <Schedule />
   </div>
 </template>
 
